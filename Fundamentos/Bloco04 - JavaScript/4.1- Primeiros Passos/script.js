45523c779c3ef03a763afb2fde51415a40566d33
Edilson Lima <edilson.lima1@hotmail.com>
2022-05-12 10:52:11 -0300

    Finalizando a parte de IF e ELSE.

--- Fundamentos/Bloco04 - JavaScript/4.1- Primeiros Passos/script.js
+++ Fundamentos/Bloco04 - JavaScript/4.1- Primeiros Passos/script.js
@@ -61,3 +61,23 @@ console.log(area);
 console.log(perimeter);
 
 // ================================================[ CONDIÇOES IF e ELSE ] ========================================================================
+// 1. Crie uma constante que receba a nota de uma pessoa candidata em um desafio técnico, e atribua a ela um valor entre 1 e 100; 
+const nota = 65;
+
+// Criterios:
+
+    // Se a nota for maior ou igual a 80, imprima "Parabéns, você foi aprovada(o)!"
+    // Se a nota for menor que 80 e maior ou igual a 60, imprima "Você está na nossa lista de espera"
+    // Se a nota for menor que 60, imprima "Você foi reprovada(o)" 
+
+// 3. Crie uma estrutura condicional utilizando o if, else if e else para criar o seu algoritmo, e os operadores lógicos que se aplicam a cada situação. 
+
+if(nota >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!")
+}else if (nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera.");
+}else{
+    console.log("Você foi reprovada(o)");
+}
+
+
