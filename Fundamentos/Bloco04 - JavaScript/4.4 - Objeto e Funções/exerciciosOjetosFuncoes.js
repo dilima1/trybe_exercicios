3b80aa5c709d722dc3c5592240f22ee9d2625076
Edilson Lima <edilson.lima1@hotmail.com>
2022-05-18 14:02:29 -0300

    Renomeiando arquivo.

--- Fundamentos/Bloco04 - JavaScript/4.4 - Objeto e Funções/exerciciosOjetosFuncoes.js
+++ Fundamentos/Bloco04 - JavaScript/4.4 - Objeto e Funções/exerciciosOjetosFuncoes.js
@@ -223,15 +223,6 @@ function somatorio(valor) {
 }
 
 somatorio(5);
-=======
-
-let maior = "";
-
-for (index = 0; index < numbers.length; index += 1) {
-  if (numbers[index] > maior) {
-    maior = numbers[index];
-  }
-}
 
 //FORMA 2 de FAZER - VIDEO GUANABARA:
 
