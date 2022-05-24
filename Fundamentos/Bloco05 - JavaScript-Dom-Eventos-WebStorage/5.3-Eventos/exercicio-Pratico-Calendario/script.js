63116788245554b26a7a9ee6661a41ab13209b39
Edilson Lima <edilson.lima1@hotmail.com>
2022-05-24 15:20:14 -0300

    construção do calendario em progresso.

--- Fundamentos/Bloco05 - JavaScript-Dom-Eventos-WebStorage/5.3-Eventos/exercicio-Pratico-Calendario/script.js
+++ Fundamentos/Bloco05 - JavaScript-Dom-Eventos-WebStorage/5.3-Eventos/exercicio-Pratico-Calendario/script.js
@@ -56,8 +56,125 @@ for (let index = 0; index < feriados.length; index += 1) {
 
 //1.3     Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday. Ex: <li class="day friday">4</li>
 
-let sextaFeiras = [14, 21, 28, 35];
+let sextaFeiras = [14, 21, 28];
 for (let index = 0; index < sextaFeiras.length; index += 1) {
   let diaTwo = sextaFeiras[index];
   holiday[diaTwo].className = "day friday";
 }
+
+// 🚀 Exercício 2:
+// Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
+//     Adicione a este botão a ID "btn-holiday".
+//     Adicione este botão como filho/filha da tag <div> com classe "buttons-container".
+
+let button = document.querySelector(".buttons-container");
+
+function createButton(string) {
+  let newButton = document.createElement("button");
+  newButton.id = "btn-holiday";
+  newButton.innerText = string;
+  newButton.addEventListener("click", feriadoTwo);
+  return newButton;
+}
+
+button.appendChild(createButton("Feriados"));
+
+//  🚀 Exercício 3:
+// Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday".
+
+//     É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)".
+
+function feriadoTwo() {
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    let classe = listDays.childNodes[index];
+    if (classe.className.includes("holiday")) {
+      // listDays.childNodes[index].style.backgroundColor = "red";
+      // listDays.childNodes[index].style.color = "white";
+      if (classe.style.backgroundColor === "red") {
+        classe.style.backgroundColor = "rgb(238,238,238)";
+      } else {
+        classe.style.backgroundColor = "red";
+      }
+    }
+  }
+}
+
+// 🚀 Exercício 4:
+// Implemente uma função que receba como parâmetro a string "Sexta-feira" e crie dinamicamente um botão com o nome "Sexta-feira".
+
+//     Adicione a este botão o ID "btn-friday".
+//     Adicione este botão como filho/filha da tag <div> com classe "buttons-container".
+let buttonFriday = document.querySelector(".buttons-container");
+
+function createButton2(string) {
+  let newButtonTwo = document.createElement("button");
+  newButtonTwo.id = "btn-friday";
+  newButtonTwo.innerText = string;
+  newButtonTwo.addEventListener("click", sextou);
+  return newButtonTwo;
+}
+
+buttonFriday.appendChild(createButton2("Sexta-Feiras"));
+
+// 🚀 Exercício 5:
+// Implemente uma função que adicione ao botão "Sexta-feira" um evento de "click" que modifica o texto exibido nos dias que são Sexta-feira.
+
+//     É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial exibindo os dias.
+function sextou() {
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    let classe = listDays.childNodes[index];
+    if (classe.className.includes("friday")) {
+      if (classe.innerText === "SEXTOU!!!") {
+        classe.innerText = classe;
+      } else {
+        classe.innerText = "SEXTOU!!!";
+      }
+    }
+  }
+}
+
+// 🚀 Exercício 6:
+// Implemente duas funções que criem um efeito de "zoom". Ao passar o ponteiro do mouse em um dia do mês no calendário, o texto desse dia deve aumentar e, quando o ponteiro do mouse sair do dia, o texto deve retornar ao tamanho original.
+
+//     Dica - Propriedade: event.target.
+
+let list = document.querySelector("#days");
+
+function aumentar(e) {
+  e.target.style.fontSize = "xx-large";
+}
+
+function diminuir(e) {
+  e.target.style.fontSize = "";
+}
+
+list.addEventListener("mouseover", aumentar, false);
+list.addEventListener("mouseout", diminuir, false);
+
+// 🚀 Exercício 7:
+// Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
+
+//     O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks".
+
+// 🚀 Exercício 8:
+// Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task.
+
+//     O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+//     O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks".
+
+// 🚀 Exercício 9:
+// Implemente uma função que adiciona um evento que, ao clicar no elemento com a tag <div> referente a cor da sua tarefa, atribua a este elemento a classe task selected, ou seja, quando sua tarefa possuir a classe task selected, ela estará selecionada.
+
+//     Ao clicar novamente no elemento, a sua classe deverá voltar a ser somente task, ou seja, esta tarefa está deixando de ser uma tarefa selecionada.
+
+// 🚀 Exercício 10:
+// Implemente uma função que adiciona um evento que, ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada.
+
+//     Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119).
+
+// 🚀 Bônus:
+// Vamos adicionar compromissos ao seu calendário? Implemente uma função que, ao digitar um compromisso na caixa de texto "COMPROMISSOS", adiciona o item à lista "MEUS COMPROMISSOS" ao clicar no botão "ADICIONAR".
+
+//     Se nenhum caractere for inserido no campo input, a função deve retornar um alert com uma mensagem de erro ao clicar em "ADICIONAR".
+//     Ao pressionar a tecla "enter" o evento também deverá ser disparado.
+//     Dica - Propriedade: key.
