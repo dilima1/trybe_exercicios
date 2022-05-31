5ca0635970ed818c8a456b25d8912c08b8cc3bc4
Edilson Lima <edilson.lima1@hotmail.com>
2022-05-31 20:37:37 -0300

    finalizando requisitos obrigatorios do exercicio pratico.

--- Fundamentos/Bloco06 - HTML-CSS-Forms-Flexbox-e-Responsivo/6.1-HTML-CSS-Forms/exercicios-praticos/script.js
+++ Fundamentos/Bloco06 - HTML-CSS-Forms-Flexbox-e-Responsivo/6.1-HTML-CSS-Forms/exercicios-praticos/script.js
@@ -1,7 +1,19 @@
 const botaoEnviar = document.getElementById("botao-enviar");
+const botaoLimpar = document.getElementById("botao-apagar");
 
 function desabilitarBotao(e) {
   e.preventDefault();
 }
 
 botaoEnviar.addEventListener("click", desabilitarBotao);
+
+function limparCampos() {
+  document.getElementById("nome-part").value = "";
+  document.getElementById("email-part").value = "";
+  document.getElementById("resposta-usuario").value = "";
+  document.getElementById("date").value = "";
+  document.getElementById("condicao1").checked = false;
+  document.getElementById("condicao2").checked = false;
+}
+
+botaoLimpar.addEventListener("click", limparCampos);
