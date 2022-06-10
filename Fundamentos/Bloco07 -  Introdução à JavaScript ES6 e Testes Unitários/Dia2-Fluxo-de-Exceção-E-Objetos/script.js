052f5ef3e6068c04f1dbcdb12d8ed44c31b757d4
Edilson Lima <edilson.lima1@hotmail.com>
2022-06-10 14:14:32 -0300

    adicionado resolução dos exercicios

--- Fundamentos/Bloco07 -  Introdução à JavaScript ES6 e Testes Unitários/Dia2-Fluxo-de-Exceção-E-Objetos/script.js
+++ Fundamentos/Bloco07 -  Introdução à JavaScript ES6 e Testes Unitários/Dia2-Fluxo-de-Exceção-E-Objetos/script.js
@@ -22,22 +22,22 @@
 
 // console.log(sum2(2, '3'));
 
-// const verifyIsNumber = (value1, value2) => {
-//   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
-//     throw new Error('Os valores devem ser numéricos');
-//   }
-// };
+const verifyIsNumber = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    throw new Error('Os valores devem ser numéricos');
+  }
+};
 
-// const sum = (value1, value2) => {
-//   try {
-//     verifyIsNumber(value1, value2);
-//     return value1 + value2;
-//   } catch (error) {
-//     return error.message;
-//   }
-// };
+const sum = (value1, value2) => {
+  try {
+    verifyIsNumber(value1, value2);
+    return value1 + value2;
+  } catch (error) {
+    return error.message;
+  }
+};
 
-// console.log(sum(2, '3'));*/
+console.log(sum(2, '3'));*/
 
 /*//===============================[ Objetos: Parte I - Adicionando novas chaves (keys) ]========================================
 // const customer = {
