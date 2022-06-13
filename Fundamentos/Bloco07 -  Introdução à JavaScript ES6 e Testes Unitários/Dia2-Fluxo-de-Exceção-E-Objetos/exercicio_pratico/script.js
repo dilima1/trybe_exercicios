4df00fb850cb38bc6c76fbc89264d93b55f9cfb9
Edilson Lima <edilson.lima1@hotmail.com>
2022-06-13 17:15:41 -0300

    ajustes

--- Fundamentos/Bloco07 -  Introdução à JavaScript ES6 e Testes Unitários/Dia2-Fluxo-de-Exceção-E-Objetos/exercicio_pratico/script.js
+++ Fundamentos/Bloco07 -  Introdução à JavaScript ES6 e Testes Unitários/Dia2-Fluxo-de-Exceção-E-Objetos/exercicio_pratico/script.js
@@ -1,22 +1,24 @@
-// function sum() {
-//       const value1 = document.getElementById('value1').value;
-//       const value2 = document.getElementById('value2').value;
-//       if(value1 === "" || value2 === ""){
-//         throw new Error('Os dois campos devem ser preenchidos.');
-//       }
-//       try {
-//         const result = parseInt(value1) + parseInt(value2);
-//         document.getElementById('result').innerHTML = `Resultado: ${result}`;
-//       } catch (error) {
-//         document.getElementById('result').innerHTML = `Os dois campos devem ser preenchidos.`;
-//       }
-//       document.getElementById('value1').value = '';
-//       document.getElementById('value2').value = '';
-//     }
-//     window.onload = () => {
-//       const button = document.getElementById('button');
-//       button.addEventListener('click', sum);
-//     }
+function sum() {
+      const value1 = document.getElementById('value1').value;
+      const value2 = document.getElementById('value2').value;
+      
+      try {
+        if(value1 === "" || value2 === ""){
+        throw new Error('Os dois campos devem ser preenchidos.');
+        }
+        const result = parseInt(value1) + parseInt(value2);
+        document.getElementById('result').innerHTML = `Resultado: ${result}`;
+      } catch (error) {
+        document.getElementById('result').innerHTML = `Os dois campos devem ser preenchidos.`;
+      }
+    
+      document.getElementById('value1').value = '';
+      document.getElementById('value2').value = '';
+    }
+    window.onload = () => {
+      const button = document.getElementById('button');
+      button.addEventListener('click', sum);
+    }
 
 //PARTE II - EXERCICIO PRATICO.
 const order = {
