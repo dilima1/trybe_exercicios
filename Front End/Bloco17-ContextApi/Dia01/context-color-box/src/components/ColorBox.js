b6b2dd928582da73c7a51d2ec28776905d3d4cf1
Edilson Lima <edilson.lima1@hotmail.com>
2022-09-14 20:55:10 -0300

    Adicionando conteudo exercicos anteriores

---
+++ Front End/Bloco17-ContextApi/Dia01/context-color-box/src/components/ColorBox.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/box.css';
+
+class ColorBox extends React.Component {
+  render() {
+    return(
+      <button
+        type="button"
+        className="box"
+        style={ { backgroundColor: 'blue' } }
+      >
+   
+
+     Click me to change my color!
+      </button>
+    )
+  }
+}
+
+export default ColorBox;
