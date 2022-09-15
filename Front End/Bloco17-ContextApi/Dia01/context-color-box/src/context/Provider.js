79a336c141d5be7da7368b627a2d3facb92b6f72
Edilson Lima <edilson.lima1@hotmail.com>
2022-09-15 18:15:10 -0300

    Finalizando exercicios de fixacao

---
+++ Front End/Bloco17-ContextApi/Dia01/context-color-box/src/context/Provider.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ColorsContext from './ColorContext';
+
+class Provider extends Component {
+  state = {
+      colors: ['blue', 'red', 'yellow'],
+      colorIndex: 0,
+    };
+  
+nextColor = () => {
+    const { colorIndex, colors } = this.state;
+    if (colorIndex < (colors.length - 1)) {
+      this.setState((prevState) => ({ colorIndex: prevState.colorIndex + 1 }));
+    } else {
+      this.setState({ colorIndex: 0 });
+    }
+}
+
+
+ render() {
+    const context = {
+      ...this.state,
+      nextColor: this.nextColor,
+    };
+
+    const { children } = this.props;
+
+    return (
+      <ColorsContext.Provider value={ context }>
+        {children}
+      </ColorsContext.Provider>
+    );
+  }
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+
+export default Provider;
